ee77225ad25e5bd8489022237c52341ae178c1f5
xiaoswaii <b05106028@ntu.edu.tw>
2019-07-29 14:10:24 +0800

    [feat] project page

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -2,8 +2,21 @@
   <div class="project">
   <div class="project_img"></div>
   <div class="project_select">
-    <div class="project_select_option">
-      <button class="project_option">Malaysia</button>
+    <div class="project_country">
+      Select Region:
+    </div>
+      <a :key="country" v-for="country in countries" class="project_option">{{ country }}</a>
+  </div>
+  <div class="project_info">
+    <div class="project_list">
+      <span class="project_list_title">Malaysia</span>
+      <ul>
+        <li class="project_list_item">1</li>
+        <li class="project_list_item">2</li>
+        <li class="project_list_item">3</li>
+      </ul>
+    </div>
+    <div class="project_detail">
     </div>
   </div>
   </div>
@@ -12,7 +25,12 @@
 <script>
 export default {
   name: 'Footer',
-  props: []
+  props: [],
+  data(){
+    return {
+      countries:["Malaysia","Taiwan","Indonesia"]
+    }
+  }
 }
 </script>
 
