bdbaffc926f64e1ece3ae1ade58e863abee1e3b0
xiaoswaii <b05106028@ntu.edu.tw>
2019-09-22 17:44:51 +0800

    init product

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,31 @@
 <template>
-<div>
-  <H1>coming soon...</H1>
+<div class="product">
+  <div class="product_img"></div>
+  <div class="product_banner">
+    <div class="product_banner_word">
+    PRODUCT
+    </div>
+  </div>
+  <div class="product_main">
+    <div class="product_img_spec">
+      <div class="product_img_part">
+      <img src="../assets/img/accelerometer_plus.jpg">
+      </div>
+      <div class="product_img_name">accelerometer_plus</div>
+    </div>
+    <div class="product_img_spec">
+      <div class="product_img_part">
+      <img src="../assets/img/mems_tilt_beam.jpg">
+      </div>
+      <div class="product_img_name">mems_tilt_beam</div>
+    </div>
+    <div class="product_img_spec">
+      <div class="product_img_part">
+      <img src="../assets/img/vw_load_cell.jpg">
+      </div>
+      <div class="product_img_name">vw_load_cell</div>
+    </div>
+  </div>
 </div>
 </template>
 
@@ -11,5 +36,5 @@ export default {
 </script>
 
 <style lang="scss">
-@import "../assets/scss/nav.scss";
+@import "../assets/scss/product.scss";
 </style>
