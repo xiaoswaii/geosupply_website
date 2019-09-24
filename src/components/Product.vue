2941787ecd88f9f1b41b0c22ed2bc30e01803233
xiaoswaii <b05106028@ntu.edu.tw>
2019-09-24 13:25:58 +0800

    product page

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -7,23 +7,11 @@
     </div>
   </div>
   <div class="product_main">
-    <div class="product_img_spec">
+    <div :key="product" v-for="product in products_img" class="product_img_spec">
       <div class="product_img_part">
-      <img src="../assets/img/accelerometer_plus.jpg">
+      <img :src="product">
       </div>
       <div class="product_img_name">accelerometer_plus</div>
-    </div>
-    <div class="product_img_spec">
-      <div class="product_img_part">
-      <img src="../assets/img/mems_tilt_beam.jpg">
-      </div>
-      <div class="product_img_name">mems_tilt_beam</div>
-    </div>
-    <div class="product_img_spec">
-      <div class="product_img_part">
-      <img src="../assets/img/vw_load_cell.jpg">
-      </div>
-      <div class="product_img_name">vw_load_cell</div>
     </div>
   </div>
 </div>
@@ -32,6 +20,15 @@
 <script>
 export default {
   name: 'Product',
+  data () {
+    return {
+    products_img:["/product/01_Strain Gage.jpg","/product/02_Rebar Strain Meter.jpg","/product/03_Piezometer.jpg"
+    ,"/product/04_Earth Pressure Cell.jpg","/product/05_Concrete Stress Cell (For NATM).jpg","/product/06_Load Cell.jpg","/product/07_LVDT for Extensometer.JPG",
+    "/product/08_Inclinometer Casing.jpg","/product/09_In-Place Inclinometer.jpg","/product/10_Beam Sensor.jpg","/product/11_Tilt Sensor.jpg"
+    ,"/product/12_Minilogger with few Channel.jpg","/product/13_Datalogger.jpg","/product/14_Wireless Node.jpg","/product/15_Wireless Sensor.jpg"
+    ,"/product/16_Wireless Accelerometer.jpg"]
+    }
+  }
 }
 </script>
 
