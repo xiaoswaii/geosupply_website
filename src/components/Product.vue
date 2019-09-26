0bd3e29cf2e685fa33ac2ef353ca503b5224e95e
xiaoswaii <b05106028@ntu.edu.tw>
2019-09-26 17:08:50 +0800

    product page init

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -7,11 +7,12 @@
     </div>
   </div>
   <div class="product_main">
-    <div :key="product" v-for="product in products_img" class="product_img_spec">
+    <div :key="product" v-for="product in products" class="product_img_spec" id="product_img_spec" @click="locationContact()">
       <div class="product_img_part">
-      <img :src="product">
+      <div class="overlay">Contact us for more detail</div>
+      <img :src="product.product_img">
       </div>
-      <div class="product_img_name">accelerometer_plus</div>
+      <div class="product_img_name">{{product.product_name}}</div>
     </div>
   </div>
 </div>
@@ -22,12 +23,17 @@ export default {
   name: 'Product',
   data () {
     return {
-    products_img:["/product/01_Strain Gage.jpg","/product/02_Rebar Strain Meter.jpg","/product/03_Piezometer.jpg"
-    ,"/product/04_Earth Pressure Cell.jpg","/product/05_Concrete Stress Cell (For NATM).jpg","/product/06_Load Cell.jpg","/product/07_LVDT for Extensometer.JPG",
-    "/product/08_Inclinometer Casing.jpg","/product/09_In-Place Inclinometer.jpg","/product/10_Beam Sensor.jpg","/product/11_Tilt Sensor.jpg"
-    ,"/product/12_Minilogger with few Channel.jpg","/product/13_Datalogger.jpg","/product/14_Wireless Node.jpg","/product/15_Wireless Sensor.jpg"
-    ,"/product/16_Wireless Accelerometer.jpg"]
+    products:[{product_name:"Strain Gage",product_img:"/product/01_Strain Gage.jpg"},{product_name:"Rebar Strain Meter",product_img:"/product/02_Rebar Strain Meter.jpg"},{product_name:"Piezometer",product_img:"/product/03_Piezometer.jpg"}
+    ,{product_name:"Earth Pressure Cell",product_img:"/product/04_Earth Pressure Cell.jpg"},{product_name:"Concrete Stress Cell (For NATM)",product_img:"/product/05_Concrete Stress Cell (For NATM).jpg"},{product_name:"Load Cell",product_img:"/product/06_Load Cell.jpg"},{product_name:"LVDT for Extensometer",product_img:"/product/07_LVDT for Extensometer.JPG"},
+    {product_name:"Inclinometer Casing",product_img:"/product/08_Inclinometer Casing.jpg"},{product_name:"In-Place Inclinometer",product_img:"/product/09_In-Place Inclinometer.jpg"},{product_name:"Beam Sensor",product_img:"/product/10_Beam Sensor.jpg"},{product_name:"Tilt Sensor",product_img:"/product/11_Tilt Sensor.jpg"}
+    ,{product_name:"Minilogger",product_img:"/product/12_Minilogger with few Channel.jpg"},{product_name:"Datalogger",product_img:"/product/13_Datalogger.jpg"},{product_name:"Wireless Node",product_img:"/product/14_Wireless Node.jpg"},{product_name:"Wireless Sensor",product_img:"/product/15_Wireless Sensor.jpg"}
+    ,{product_name:"Wireless Accelerometer",product_img:"/product/16_Wireless Accelerometer.jpg"}]
     }
+  },
+  methods: {
+      locationContact(){
+        document.location.href="/ContactUs";
+      }
   }
 }
 </script>
