c004c58cfcbddb566ce49adc4043c4df5393acfa
xiaoswaii <b05106028@ntu.edu.tw>
2019-10-24 16:53:44 +0800

    index resource uglify

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -2,9 +2,9 @@
   <swiper :options="swiperOption">
         <swiper-slide><img class="swiper_img" src="../assets/img/005.jpg"></swiper-slide>
         <swiper-slide><img class="swiper_img" src="../assets/img/006.jpg"></swiper-slide>
-        <swiper-slide><img class="swiper_img" src="../assets/img/002.png"></swiper-slide>
-        <swiper-slide><img class="swiper_img" src="../assets/img/003.png"></swiper-slide>
-        <swiper-slide><img class="swiper_img" src="../assets/img/004.png"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/002.jpg"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/003.jpg"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/001.jpg"></swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-prev" slot="button-prev"></div>
         <div class="swiper-button-next" slot="button-next"></div>
