4e2e4f22426e280a3dc16e87ef4858a6bfee6d80
xiaoswaii <b05106028@ntu.edu.tw>
2019-11-05 00:29:09 +0800

    add alt to index

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,10 +1,10 @@
 <template>
   <swiper :options="swiperOption">
-        <swiper-slide><img class="swiper_img" src="../assets/img/005.jpg"></swiper-slide>
-        <swiper-slide><img class="swiper_img" src="../assets/img/006.jpg"></swiper-slide>
-        <swiper-slide><img class="swiper_img" src="../assets/img/002.jpg"></swiper-slide>
-        <swiper-slide><img class="swiper_img" src="../assets/img/003.jpg"></swiper-slide>
-        <swiper-slide><img class="swiper_img" src="../assets/img/001.jpg"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/005.jpg" alt="捷貿儀器工程有限公司，捷貿"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/006.jpg" alt="捷貿儀器工程有限公司，捷貿"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/002.jpg" alt="捷貿儀器工程有限公司，捷貿"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/003.jpg" alt="捷貿儀器工程有限公司，捷貿"></swiper-slide>
+        <swiper-slide><img class="swiper_img" src="../assets/img/001.jpg" alt="捷貿儀器工程有限公司，捷貿"></swiper-slide>
         <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-prev" slot="button-prev"></div>
         <div class="swiper-button-next" slot="button-next"></div>
--- src/components/Index.vue
+++ src/components/Index.vue
@@ -7,7 +7,7 @@
       </div>
     </div>
       <div class="aboutus_index">
-          <div class="aboutus_left"><img src="../assets/img/aboutus_index.jpg"></div>
+          <div class="aboutus_left"><img src="../assets/img/aboutus_index.jpg" alt="捷貿儀器工程有限公司，捷貿，首頁"></div>
           <div class="aboutus_right" data-aos="fade-in">
             <span class="triangle"></span>
             <span class="aboutus_right_title">{{$t('index.about.title')}}</span>
@@ -22,10 +22,10 @@
       <div class="product_index">
         <div class="product_index_title">{{$t('index.product.title')}}</div>
         <div class="product_index_content">
-          <div class="product_index_img"><img src="/product/09_In-Place Inclinometer.jpg"><div class="inner_product">{{$t('index.product.product1')}}</div></div>
-          <div class="product_index_img"><img src="/product/13_Datalogger.jpg"><div class="inner_product">{{$t('index.product.product2')}}</div></div>
-          <div class="product_index_img"><img src="/product/14_Wireless Node.jpg"><div class="inner_product">{{$t('index.product.product3')}}</div></div>
-          <div class="product_index_img"><img src="../assets/img/product/17_CloudSoftware.jpg"><div class="inner_product">{{$t('index.product.product4')}}</div></div>
+          <div class="product_index_img"><img src="/product/09_In-Place Inclinometer.jpg" alt="捷貿儀器工程有限公司，捷貿"><div class="inner_product">{{$t('index.product.product1')}}</div></div>
+          <div class="product_index_img"><img src="/product/13_Datalogger.jpg" alt="捷貿儀器工程有限公司，捷貿"><div class="inner_product">{{$t('index.product.product2')}}</div></div>
+          <div class="product_index_img"><img src="/product/14_Wireless Node.jpg" alt="捷貿儀器工程有限公司，捷貿"><div class="inner_product">{{$t('index.product.product3')}}</div></div>
+          <div class="product_index_img"><img src="../assets/img/product/17_CloudSoftware.jpg" alt="捷貿儀器工程有限公司，捷貿"><div class="inner_product">{{$t('index.product.product4')}}</div></div>
         </div>
       </div>
       <div class="service_index" data-aos="fade-left" data-aos-delay="50"
@@ -45,16 +45,16 @@
             </div>
           </div>
           <div class="service_right">
-            <img src="../assets/img/service.jpg">
+            <img src="../assets/img/service.jpg" alt="捷貿儀器工程有限公司，捷貿">
           </div>
       </div>
       <div class="project_index" data-aos="fade-up" data-aos-delay="100"
     data-aos-duration="1000">
         <span class="project_index_title">{{ $t('index.project.title') }}</span>
         <div class="project_index_content">
-          <div class="project_index_img" id="project_taiwan" @click="goProject('Taiwan')"><img src="../assets/img/taiwan.jpg"><div class="inner">{{ $t('index.project.taiwan') }}</div></div>
-          <div class="project_index_img" id="project_malaysia" @click="goProject('Malaysia')"><img src="../assets/img/malaysia.jpg"><div class="inner">{{ $t('index.project.malaysia') }}</div></div>
-          <div class="project_index_img" id="project_indonesia" @click="goProject('Indonesia')"><img src="../assets/img/indonesia.jpg"><div class="inner">{{ $t('index.project.indonesia') }}</div></div>
+          <div class="project_index_img" id="project_taiwan" @click="goProject('Taiwan')"><img src="../assets/img/taiwan.jpg" alt="捷貿儀器工程有限公司，台灣"><div class="inner">{{ $t('index.project.taiwan') }}</div></div>
+          <div class="project_index_img" id="project_malaysia" @click="goProject('Malaysia')"><img src="../assets/img/malaysia.jpg" alt="捷貿儀器工程有限公司，馬來西亞"><div class="inner">{{ $t('index.project.malaysia') }}</div></div>
+          <div class="project_index_img" id="project_indonesia" @click="goProject('Indonesia')"><img src="../assets/img/indonesia.jpg" alt="捷貿儀器工程有限公司，印尼"><div class="inner">{{ $t('index.project.indonesia') }}</div></div>
         </div>
         <span class="read_more"><a id="readmore" @click="readmore()">{{ $t('index.project.read') }}</a></span>
       </div>
